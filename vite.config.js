1854f56339980e98e698acd5777d393a288f518d
sachin915t <sachinmaurya747@gmail.com>
2026-02-28 21:12:40 +0530

    web vite install app feat

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Logan-stream',
+        short_name: 'Logan',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/public/icon1.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/public/icon2.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
